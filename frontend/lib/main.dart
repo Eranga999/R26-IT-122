6ace6b82f495adcc39e07703ae92c13a2ec1f929
Eranga999 <it22253644@my.sliit.lk>
2026-02-26 17:42:16 +0530

    update main dart file

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,19 +1,9 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'core/theme/app_theme.dart';
 import 'features/splash/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Desktop SQLite support (Windows / Linux / macOS)
-  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-    sqfliteFfiInit();
-    databaseFactory = databaseFactoryFfi;
-  }
-
   runApp(const HeritageArApp());
 }
 
